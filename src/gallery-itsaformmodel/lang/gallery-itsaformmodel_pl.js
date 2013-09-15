02abd4822a05ad2ef4983f0c3fa4eee4603cdb97
Marco Asbreuk <marco@itsasbreuk.nl>
2013-09-15 14:23:18 +0200

    Added Polish language

---
+++ src/gallery-itsaformmodel/lang/gallery-itsaformmodel_pl.js
@@ -0,0 +1,6 @@
+{
+    notification : 'zgłoszenie',
+    datachanged : 'Dane zostały zmienione poza formie',
+    wantreload : 'Włóż go w formie',
+    noreloadmsg : 'jeśli nie, to dane zostaną przywrócone do aktualnych wartościach formularzy'
+}
