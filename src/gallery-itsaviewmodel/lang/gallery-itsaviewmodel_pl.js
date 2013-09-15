dc841391ae4ff02023f19eaefddecf77e5a462c3
Marco Asbreuk <marco@itsasbreuk.nl>
2013-09-15 14:24:53 +0200

    Adde Polish language

---
+++ src/gallery-itsaviewmodel/lang/gallery-itsaviewmodel_pl.js
@@ -0,0 +1,16 @@
+{
+    cancel: 'anulować',
+    destroy: 'zniszczyć',
+    remove: 'usunąć',
+    reset: 'zresetować',
+    save: 'oszczędzać',
+    submit: 'przedkładać',
+    load: 'załadować',
+    abort: 'poronić',
+    ignore: 'ignorować',
+    retry: 'spróbować ponownie',
+    ok: 'dobrze',
+    yes: 'tak',
+    no: 'nie',
+    undefined_template: 'szablon jest niezdefiniowane'
+}
