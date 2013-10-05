3a887486a53987dedd4bc1b97140c580ec4cbebf
Marco Asbreuk <marco@itsasbreuk.nl>
2013-10-05 16:44:50 +0200

    Initial commit

---
+++ src/gallery-itsamessage/js/gallery-itsamessage.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/*jshint maxlen:200 */
+
+/**
+ *
+ * Extends Y.Model by adding methods through which they can create editable form-elements, which represent and are bound to the propery-value.
+ * This model is for defining the UI-structure for all Model's properties and for firing model-events for
+ * Y.ITSAFormModel does not rendering to the dom itself. That needs to be done by an Y.View-instance, like Y.ITSAViewModel.
+ *
+ * @module gallery-itsaformmodel
+ * @extends Model
+ * @uses gallery-itsamodelsyncpromise
+ * @class ITSAFormModel
+ * @constructor
+ * @since 0.1
+ *
+ * <i>Copyright (c) 2013 Marco Asbreuk - http://itsasbreuk.nl</i>
+ * YUI BSD License - http://developer.yahoo.com/yui/license.html
+ *
+*/
+
+var MESSAGETYPES = {
+        message: true,
+        warning: true,
+        error: true,
+        dialog: true,
+        input: true
+    };
+
+ITSAMessage = Y.ITSAMessage = Y.Base.create('itsamessage', Y.FormModel, [], {}, {
+    ATTRS: {
+        footer: {
+            writeOnce: 'initOnly',
+            validator: function(v) {
+                return (typeof v==='string');
+            }
+        },
+        message: {
+            writeOnce: 'initOnly',
+            validator: function(v) {
+                return (typeof v==='string');
+            }
+        },
+        options: {
+            readOnly: true,
+            validator: function(v) {
+                return (typeof v==='object');
+            }
+        },
+        processing: {
+            readOnly: true,
+            value: false,
+            validator: function(v) {
+                return (typeof v==='boolean');
+            }
+        },
+        source: {
+            writeOnce: 'initOnly',
+            validator: function(v) {
+                return (typeof v==='string');
+            }
+        },
+        title: {
+            writeOnce: 'initOnly',
+            validator: function(v) {
+                return (typeof v==='string');
+            }
+        },
+        type: {
+            writeOnce: 'initOnly',
+            validator: function(v) {
+                return (typeof v==='string') && MESSAGETYPES[v];
+            }
+        }
+    }
+});
+
+ITSAFormModel.prototype._widgetValueFields = {}; // private prototypeobject can be filled by setWidgetValueField()
