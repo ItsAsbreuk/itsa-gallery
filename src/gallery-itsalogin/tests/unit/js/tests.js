841712c309e6d112e062a9235fe8597fad6fbc70
Marco Asbreuk <marco@itsasbreuk.nl>
2013-11-25 13:21:31 +0100

    added unittest

--- src/gallery-itsalogin/tests/unit/js/tests.js
+++ src/gallery-itsalogin/tests/unit/js/tests.js
@@ -5,12 +5,25 @@ YUI.add('module-tests', function(Y) {
     suite.add(new Y.Test.Case({
         name: 'Automated Tests',
         'test is empty': function() {
-            var d = new Y.Dial();
-            Y.Assert.fail('No Tests Provided For This Module');
+            Y.getLogin('Login', 'Please enter login', {
+                imageButtons: true,
+                showStayLoggedin: true,
+                regain: 'usernameorpassword'
+            }, null).then(
+                function(result) {
+                    Y.showMessage('Resolved:', 'succesfully logged in with username='+result.username+
+                        ' | password='+result.password+' | stayloggedin: '+result.remember+
+                        ' | button='+result.button);
+                },
+                function(reason) {
+                    Y.showMessage('Rejected:', reason);
+                }
+            );
+            this.wait(20000);
         }
     }));
 
     Y.Test.Runner.add(suite);
 
 
-},'', { requires: [ 'test', 'dial' ] });
+},'', { requires: [ 'test', 'gallery-itsalogin' ] });
