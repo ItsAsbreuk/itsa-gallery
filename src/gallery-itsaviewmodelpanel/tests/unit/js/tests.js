235358f95e4a20f5bd203446ff2a637314da4d13
Marco Asbreuk <marco@itsasbreuk.nl>
2013-11-25 12:26:26 +0100

    updated unittest

--- src/gallery-itsaviewmodelpanel/tests/unit/js/tests.js
+++ src/gallery-itsaviewmodelpanel/tests/unit/js/tests.js
@@ -20,70 +20,21 @@ test3: is a form NOT generated when editable elements and the container is a for
             this.viewmodel2 = new Y.ITSAViewModelPanel();
             this.viewmodel3 = new Y.ITSAViewModelPanel();
             this.viewmodel4 = new Y.ITSAViewModelPanel();
-            this.viewmodel5 = new Y.ITSAViewModelPanel();
-            this.viewmodel6 = new Y.ITSAViewModelPanel();
-            this.viewmodel7 = new Y.ITSAViewModelPanel();
-            this.viewmodel8 = new Y.ITSAViewModelPanel();
-            this.viewmodel9 = new Y.ITSAViewModelPanel();
-            this.viewmodel10 = new Y.ITSAViewModelPanel();
-            this.viewmodel11 = new Y.ITSAViewModelPanel();
-            this.viewmodel12 = new Y.ITSAViewModelPanel();
-            this.viewmodel13 = new Y.ITSAViewModelPanel();
-            this.viewmodel14 = new Y.ITSAViewModelPanel();
-            this.viewmodel15 = new Y.ITSAViewModelPanel();
-            this.viewmodel16 = new Y.ITSAViewModelPanel();
-            this.viewmodel17 = new Y.ITSAViewModelPanel();
-            this.viewmodel18 = new Y.ITSAViewModelPanel();
-            this.viewmodel19 = new Y.ITSAViewModelPanel();
-            this.viewmodel20 = new Y.ITSAViewModelPanel();
         },
         tearDown : function () {
             this.viewmodel.destroy();
             this.viewmodel2.destroy();
             this.viewmodel3.destroy();
             this.viewmodel4.destroy();
-            this.viewmodel5.destroy();
-            this.viewmodel6.destroy();
-            this.viewmodel7.destroy();
-            this.viewmodel8.destroy();
-            this.viewmodel9.destroy();
-            this.viewmodel10.destroy();
-            this.viewmodel11.destroy();
-            this.viewmodel12.destroy();
-            this.viewmodel13.destroy();
-            this.viewmodel14.destroy();
-            this.viewmodel15.destroy();
-            this.viewmodel16.destroy();
-            this.viewmodel17.destroy();
-            this.viewmodel18.destroy();
-            this.viewmodel19.destroy();
-            this.viewmodel20.destroy();
         },
         'test GC': function() {
             this.viewmodel.render();
             this.viewmodel2.render();
             // no rendering viewmodel3, because that leads to different destruction
             this.viewmodel4.render();
-            this.viewmodel5.render();
-            this.viewmodel6.render();
-            this.viewmodel7.render();
-            this.viewmodel8.render();
-            this.viewmodel9.render();
-            this.viewmodel10.render();
-            this.viewmodel11.render();
-            this.viewmodel12.render();
-            this.viewmodel13.render();
-            this.viewmodel14.render();
-            this.viewmodel15.render();
-            this.viewmodel16.render();
-            this.viewmodel17.render();
-            this.viewmodel18.render();
-            this.viewmodel19.render();
-            this.viewmodel20.render();
             Y.Assert.pass();
         }
     }));
-/*
     suite.add(new Y.Test.Case({
         name: 'Memory Tests with model',
         setUp : function () {
@@ -146,6 +97,27 @@ test3: is a form NOT generated when editable elements and the container is a for
                 release: 1980
             });
             this.artist.setLifeUpdate(true);
+            this.artist2 = new Y.ArtistModel({
+                name: 'U2',
+                country: 'Ireland',
+                firstalbum: 'Boy',
+                release: 1980
+            });
+            this.artist2.setLifeUpdate(true);
+            this.artist3 = new Y.ArtistModel({
+                name: 'U2',
+                country: 'Ireland',
+                firstalbum: 'Boy',
+                release: 1980
+            });
+            this.artist3.setLifeUpdate(true);
+            this.artist4 = new Y.ArtistModel({
+                name: 'U2',
+                country: 'Ireland',
+                firstalbum: 'Boy',
+                release: 1980
+            });
+            this.artist4.setLifeUpdate(true);
 
             var template = '<fieldset>'+
                             '<legend>Editable template</legend>'+
@@ -162,12 +134,17 @@ test3: is a form NOT generated when editable elements and the container is a for
                 editable: true
             });
             this.viewmodel2 = new Y.ITSAViewModelPanel({
-                model: this.artist,
+                model: this.artist2,
                 template: template,
                 editable: true
             });
             this.viewmodel3 = new Y.ITSAViewModelPanel({
-                model: this.artist,
+                model: this.artist3,
+                template: template,
+                editable: true
+            });
+            this.viewmodel4 = new Y.ITSAViewModelPanel({
+                model: this.artist4,
                 template: template,
                 editable: true
             });
@@ -176,16 +153,20 @@ test3: is a form NOT generated when editable elements and the container is a for
             this.viewmodel.destroy();
             this.viewmodel2.destroy();
             this.viewmodel3.destroy();
+            this.viewmodel4.destroy();
             this.artist.destroy();
+            this.artist2.destroy();
+            this.artist3.destroy();
+            this.artist4.destroy();
         },
         'test GC2': function() {
             this.viewmodel.render();
             this.viewmodel2.render();
            // this.viewmodel3.render();
+            this.viewmodel4.render();
             Y.Assert.pass();
         }
     }));
-*/
     Y.Test.Runner.add(suite);
 
 
