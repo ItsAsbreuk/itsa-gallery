2aa399963bea686402fd581d887f0bdd9306d5e8
Marco Asbreuk <marco@itsasbreuk.nl>
2014-01-29 01:27:46 +0100

    initial commit

---
+++ src/gallery-itsalevelbarrel/js/gallery-itsalevelbarrel.js
@@ -0,0 +1,241 @@
+'use strict';
+/*jshint maxlen:205 */
+
+/**
+ *
+ * This module adds some static methods to the Y.Node class that can be used to controll node-availabilities.<br />
+ *
+ * @module gallery-itsalevelbarrel
+ * @extends Widget
+ * @class Y.Plugin.ITSALevelBarrel
+ * @since 0.1
+ *
+ * <i>Copyright (c) 2013 Marco Asbreuk - http://theinternetwizard.net</i>
+ * YUI BSD License - http://developer.yahoo.com/yui/license.html
+ *
+*/
+
+var Lang = Y.Lang,
+    YArray = Y.Array,
+    VALUE = 'value',
+    MAXVALUE = 'maxValue',
+    CAP_CHANGE = 'Change',
+    OLOR = 'olor',
+    COLOR = 'c'+OLOR,
+    CAP_COLOR = 'C'+OLOR,
+    LINECOLOR = 'line'+CAP_COLOR,
+    BACKGROUNDCOLOR = 'background'+CAP_COLOR,
+    BOUNDINGBOX = 'boundingBox',
+    CONTENTBOX = 'contentBox',
+    LEVEL_INDICATOR = 'level-indicator',
+    CONTENT_INNER_TEMPLATE = '<div class="barrelvalue">{value}</div><div class="'+LEVEL_INDICATOR+'"></div>',
+    ITSA_BARREL_CLASS = 'itsa-barrel-container';
+
+
+function ITSALevelBarrel() {
+    ITSALevelBarrel.superclass.constructor.apply(this, arguments);
+}
+
+ITSALevelBarrel.NAME = 'itsalevelbarrel';
+
+ITSALevelBarrel.ATTRS = {
+
+    /**
+     * Auto-number the marker-html - when no markerHTML is specified.
+     *
+     * @attribute value
+     * @type {number}
+     * @default 0
+     * @since 0.1
+     */
+    value: {
+        value: 0,
+        validator: function(v){ return (typeof v === 'number'); }
+    },
+
+    /**
+     *
+     * @attribute maxValue
+     * @type {number}
+     * @default 100
+     * @since 0.1
+     */
+    maxValue: {
+        value: 100,
+        validator: function(v){ return (typeof v === 'number'); }
+    },
+
+    /**
+     * Array with all the checked options. The Array is an Array of String-types which are present in 'options' and checked.
+     *
+     * @attribute color
+     * @type {String}
+     * @default null
+     * @since 0.1
+     */
+    color: {
+        value: null,
+        validator: function(v){ return (v===null) || (typeof v === 'string'); }
+    },
+
+    /**
+     * Default colorclass. These are predefined css-classnames you could use (or define one of your own):
+     * 'red'  'blue'
+     *
+     * @attribute backgroundColor
+     * @type {String}
+     * @default null
+     * @since 0.1
+     */
+    backgroundColor: {
+        value: null,
+        validator: function(v){ return (v===null) || (typeof v === 'string'); }
+    },
+
+    /**
+     * Free margin of the map [px] where the markers should stay within when 'markersInView' or 'centerView' is called.
+     *
+     * @attribute lineColor
+     * @type {String}
+     * @default null
+     * @since 0.1
+     */
+    lineColor: {
+        value: null,
+        validator: function(v){ return (v===null) || (typeof v === 'string'); }
+    }
+
+};
+
+Y.ITSALevelBarrel = Y.extend(ITSALevelBarrel, Y.Widget);
+
+/**
+ * @method initializer
+ * @protected
+ * @since 0.1
+*/
+ITSALevelBarrel.prototype.initializer = function() {
+    Y.log('initializer', 'info', 'ITSALevelBarrel');
+    var instance = this;
+};
+
+/**
+ * Creates the containerdiv that holds all the markers
+ *
+ * @method renderUI
+ * @protected
+ * @since 0.1
+*/
+ITSALevelBarrel.prototype.renderUI = function() {
+    Y.log('renderUI', 'info', 'ITSALevelBarrel');
+    var instance = this,
+        boundingBox = instance.get(BOUNDINGBOX),
+        contentBox = instance.get(CONTENTBOX);
+    boundingBox.addClass(ITSA_BARREL_CLASS);
+    contentBox.setHTML(Lang.sub(CONTENT_INNER_TEMPLATE, {value: instance.get(VALUE)}));
+};
+
+/**
+ * Creates eventlistens
+ *
+ * @method bindUI
+ * @protected
+ * @since 0.1
+*/
+ITSALevelBarrel.prototype.bindUI = function() {
+    Y.log('bindUI', 'info', 'ITSALevelBarrel');
+    var instance = this,
+        eventhandlers;
+    eventhandlers = instance._eventhandlers = [];
+    eventhandlers.push(
+        instance.after(
+            [VALUE+CAP_CHANGE, MAXVALUE+CAP_CHANGE],
+            Y.bind(instance.syncUI, instance)
+        )
+    );
+    eventhandlers.push(
+        instance.after(
+            [COLOR+CAP_CHANGE],
+            Y.bind(instance._setColor, instance)
+        )
+    );
+    eventhandlers.push(
+        instance.after(
+            [LINECOLOR+CAP_CHANGE],
+            Y.bind(instance._setLineColor, instance)
+        )
+    );
+    eventhandlers.push(
+        instance.after(
+            [BACKGROUNDCOLOR+CAP_CHANGE],
+            Y.bind(instance._setBackgroundColor, instance)
+        )
+    );
+};
+
+/**
+ * syncs the whole modellist into
+ * @method syncUI
+ * @protected
+ * @since 0.1
+*/
+ITSALevelBarrel.prototype.syncUI = function(initialize) {
+    Y.log('syncUI', 'info', 'ITSALevelBarrel');
+    var instance = this,
+        contentBox = instance.get(CONTENTBOX),
+        levelBox = contentBox.one('.'+LEVEL_INDICATOR),
+        value = instance.get(VALUE),
+        maxvalue = instance.get(MAXVALUE),
+        percentedHeight = Math.round(100*(maxvalue-value)/maxvalue);
+    levelBox.setStyle('top', percentedHeight+'%');
+};
+
+/**
+ * Cleans up bindings
+ *
+ * @method destructor
+ * @protected
+ * @since 0.3
+*/
+ITSALevelBarrel.prototype.destructor = function() {
+    Y.log('destructor', 'info', 'ITSALevelBarrel');
+    var instance = this;
+    instance._clearEventhandlers();
+};
+
+
+/**
+ * Cleaning up all eventlisteners
+ *
+ * @method _clearEventhandlers
+ * @private
+ * @since 0.3
+ *
+*/
+ITSALevelBarrel.prototype._clearEventhandlers = function() {
+    Y.log('_clearEventhandlers', 'info', 'ITSALevelBarrel');
+    YArray.each(
+        this._eventhandlers,
+        function(item){
+            item.detach();
+        }
+    );
+};
+
+/**
+ * Sets the backgroundcolor of the marker
+ *
+ * @method _setBackgroundColor
+ * @private
+ * @since 0.3
+ *
+*/
+ITSALevelBarrel.prototype._setBackgroundColor = function() {
+    Y.log('_setBackgroundColor', 'info', 'ITSALevelBarrel');
+    YArray.each(
+        this._eventhandlers,
+        function(item){
+            item.detach();
+        }
+    );
+};
